24ef4bcb50e32119973dba055114d6555f8ae3b3
Anderson Deizepe <anderson_deizepe@yahoo.com.br>
2019-08-22 18:42:57 -0300

    Alteração  resources/config/config.php para .env

--- src/resources/config/config.php
+++ src/resources/config/config.php
@@ -7,9 +7,9 @@ return [
         'Accept-Language: pt-br;q=0.9,pt-BR'
     ],
     'defaults'  => [
-        'publishable_key'   => 'YOUR_PUBLISHABLE_KEY',
-        'marketplace_id'    => 'YOUR_MARKETPLACE_ID',
-        'endpoint'          => 'https://api.zoop.ws',
+        'publishable_key'   => env('ZOOP_PUBLISHABLE_KEY', ''), 
+        'marketplace_id'    => env('ZOOP_MARKETPLACE_ID', ''),
+        'endpoint'          => env('ZOOP_API', 'https://api.zoop.ws'),
         'api_version'       => 'v1',
     ]
 ];
